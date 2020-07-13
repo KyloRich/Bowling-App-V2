758de31bc489e9e3e0ba9d4d73552aa3463e1475
Squirrel Master <22codingsm@gmail.com>
2020-07-12 18:35:07 -0700

    Minor Error fixes
    - Corrected a few errors
    
    - Still needs work on Bowler class. Need to get the logic for adding bowler locked down.  Ran out of time today.

--- eclipseEE-workspace-Bowler-V2/bowler-app/src/main/java/com/codingsm/bowler_app/Game.java
+++ eclipseEE-workspace-Bowler-V2/bowler-app/src/main/java/com/codingsm/bowler_app/Game.java
@@ -3,153 +3,76 @@ package com.codingsm.bowler_app;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Game {
-	// ArrayList for the RegularFrames
-	private static ArrayList<RegularFrame> game = new ArrayList<>();
-	// Keep track of the RegularFrame
-	private int RegularFrame = 1;
-	// Create a Scanner object
-	Scanner sc = new Scanner(System.in); 
-	// Determine if game is over
-	private boolean isOver = false; 
+public class Bowler {
 	
-	 
+	Scanner sc = new Scanner(System.in);
+	private static ArrayList<Frame> game = new ArrayList<>();
+	String name;
+	String lastName = null;
+	int score;
 	
-	public void startGame() {
-		// TODO logic for adding bowlers with arraylist of bowlers -- Kyle still working on it.
-		ArrayList<String> bowlerList = new ArrayList<>();
-		Scanner sc = new Scanner(System.in);
 
-		// ***Kyle Additions***//
-		do {
-			System.out.println("Welcome to WyleTech Bowling! \nPlease enter your name");
-			bowlerList.add(sc.next());
-			// prompt for next player
-			System.out.println("Would you like to add a player? yes or no?");
-			// While loop for adding players if user enters a char "y"
-		} while (sc.next().startsWith("y"));
-		startGame();
-
-		// If statement, determining solo or multi-player game
-		if (sc.next().startsWith("n")); {
-			System.out.println("Have fun bowling!");
-			setGame();
-		}
+	//***Kyle Additions//
+	Bowler(String name, String lastName, int score){
+		this.name = name;
+		
+		//TEMP COMMENT EXIT TO TEST GAME CLASS CAll
+		//this.score = score;
+		//this.lastName = lastName;
+	}
+	
+//	private void updateScore() {
+//		//TODO
+//		
+//		String ball1, ball2;
+//		try {
+//			System.out.println("Enter pins for Frame #" + RegularFrame.getFrame() + " Ball #1 ");
+//			System.out.println("[X = Strike]");
+//			// Grab the input from the user
+//			ball1 = sc.nextLine().toUpperCase();
+//			if (!ball1.equals("X")) {
+//				System.out.println("Enter pins for Frame #" + RegularFrame.getFrame() + " Ball #2 ");
+//				// Get the second ball from the user
+//				ball2 = sc.nextLine();
+//			}
+//			else {
+//				ball2 = " ";
+//			}
+//			game.add(new RegularFrame(ball1.charAt(0),ball2.charAt(0)));
+//			RegularFrame.setFrame(++);
+//		} catch (FrameException e) {
+//			System.out.println("Could not create RegularFrame, " + e.getMessage());
+//		}
+//	}
+	
+	//****Kyle Edits**** Getters and Setters for Name - 
+	public String getName() {
+		return name;
 	}
 
-	//***Kyle Additions***//
-	private String addPlayers() {
-		//TODO 
-		String numOfPlayers;
-	    System.out.println("How many players will be bowling?");
-	    numOfPlayers = sc.next();
-	    
-	    return numOfPlayers; 
-	
+	public void setName(String name) {
+		this.name = name;
 	}
-	
-	// Start the game from the main()
-	public void setGame() {
-		
-		
-		//TODO Logic to loop through each bowler assigning one frame of balls
-		
-		
-		// Loop through 10 RegularFrames
-		while (!isOver) {
-			System.out.println("Now bowling in RegularFrame # "+ RegularFrame);
-			// Set up 11 and 12 RegularFrame
-			if (RegularFrame == 11)
-				getScoreExtra();
-			// Get the score from the user's input
-			else
-				//getScore();
-			// Show the score after every RegularFrame
-			showScore();
-		}
-		System.out.println(" GAME OVER! ");
+
+	public String getLastName() {
+		return lastName;
 	}
-	
-	private void getScoreExtra() {
-		// Check if last RegularFrame was a spare or strike
-		if(game.get(9).isSpare) {
-			try {
-				// Since a spare only get one ball
-				System.out.println("You get one extra ball.");
-				System.out.println("[X = Strike]");
-				String ball1 = sc.nextLine().toUpperCase();
-				game.add(new RegularFrame(ball1.charAt(0),'0'));
-				RegularFrame++;
-				// Finish the game
-				isOver = true;
-			} catch (FrameException e) {
-				System.out.println("Could not create Frame, " + e.getMessage());
-			}			
-			return;
-		}
-		// Strike gets two balls
-		try {
-			System.out.println("You get two extra balls.");
-			System.out.println("Enter your first ball.");
-			System.out.println("[X = Strike]");
-			String ball1 = sc.nextLine().toUpperCase();
-			System.out.println("Enter your second ball.");
-			System.out.println("[X = Strike]");
-			String ball2 = sc.nextLine().toUpperCase();
-			// If a strike will need to add two RegularFrames
-			if (ball1.charAt(0) == 'X') {
-				game.add(new RegularFrame(ball1.charAt(0),' '));
-				game.add(new RegularFrame(ball2.charAt(0),' '));
-			}
-			else {
-				game.add(new RegularFrame(ball1.charAt(0),ball2.charAt(0)));				
-			}
-			RegularFrame++;
-			// Finish the game
-			isOver = true;
-		} catch (FrameException e) {
-			System.out.println("Could not create RegularFrame, " + e.getMessage());
-		}
-		return;
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	
-	
-	
-	private void showScore() {
-		System.out.println(" 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |");
-		System.out.println("-----------------------------------------");
-		// Loop through the entire array
-		
-		for (int x = 0; x < game.size(); x++) {
-			if (game.get(x).isStrike)
-				System.out.print( "  X ");
-			else if (game.get(x).isSpare)
-				System.out.print( game.get(x).b1 + " / ");
-			else {
-				System.out.print( game.get(x).b1 + " " + game.get(x).b2 + " ");
-				if (RegularFrame == 11) 
-					isOver = true;
-			}
-		}
-		System.out.println();
-		System.out.println("\n Game Total = " + calculateScore());
-	}
-	
-	private int calculateScore() {
-		int score = 0;
-		
-		for (int x = 0; x < game.size(); x++) {
-			if (game.get(x).isSpare && x < game.size()-1)
-				score += game.get(x+1).b1 + 10; 
-			else if (game.get(x).isStrike && x < game.size()-1 && !game.get(x+1).isStrike)
-				score += game.get(x+1).b1 + game.get(x+1).b2 + 10; 
-			else if (game.get(x).isStrike && (x+1) < game.size()-1 && game.get(x+1).isStrike)
-				score += game.get(x+2).b1 + 20; 
-			else if (!game.get(x).isSpare && !game.get(x).isStrike)
-				score += game.get(x).b1 + game.get(x).b2; 
-			
-		}
+
+	public int getScore() {
 		return score;
 	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
 	
 }
+
+
+
+
